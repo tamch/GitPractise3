4d3fefd3b32f715941047b499259ad49f233c08c
tamch <tam@medias-in-res.com>
2017-04-26 15:59:13 +0800

    function

--- GitPractise3/GitPractise3/ViewController.swift
+++ GitPractise3/GitPractise3/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func testFunction1 () {
+        print ("test 1")
     }
-
-
+    
+    func testFunction2 () {
+        print ("test 2")
+    }
 }
-
